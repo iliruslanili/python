a6073418d715b62b8a40ef9e252564ddb4af5e82
iliruslanili <73334548+iliruslanili@users.noreply.github.com>
2021-08-15 00:53:03 +0300

    Delete task-2.py

--- task-2.py
+++
@@ -1,8 +0,0 @@
-# 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
-# Используйте форматирование строк.
-
-seconds = int(input('Введите время в секундах: '))
-minutes = seconds // 60 if seconds // 60 < 60 else seconds % 60
-total_minutes = seconds // 60
-hours = total_minutes // 60
-print(f'{hours}:{minutes:02}:{seconds%60:02}')
